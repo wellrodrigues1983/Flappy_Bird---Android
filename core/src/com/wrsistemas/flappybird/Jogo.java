7f21eea9b4a4fcead3058d7ff6909a683a4c9569
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-11-30 19:04:27 -0300

    Adicionando Canos

--- core/src/com/wrsistemas/flappybird/Jogo.java
+++ core/src/com/wrsistemas/flappybird/Jogo.java
@@ -12,11 +12,12 @@ import java.util.List;
 
 public class Jogo extends ApplicationAdapter {
 
-	private int movimentoX = 0;
-	private int movimentoY = 0;
+	//Texturas
 	private SpriteBatch batch;
 	private List<Texture> passaros = new ArrayList<>();
 	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canoTopo;
 
 	//Atributos de configuração
 	private float larguraDispositivos;
@@ -24,29 +25,26 @@ public class Jogo extends ApplicationAdapter {
 	private float variacao = 0;
 	private float gravidade = 0;
 	private float posicaoInicialVertical = 0;
+	private float posicaoCanoHorizontal;
+	private float espacoEntreCanos;
 
 	@Override
 	public void create () {
-		/*Gdx.app.log("create", "jogo iniciado");*/
-
-		batch = new SpriteBatch();
-		passaros.add(new Texture("passaro1.png"));
-		passaros.add(new Texture("passaro2.png"));
-		passaros.add(new Texture("passaro3.png"));
-		fundo = new Texture("fundo.png");
-
-		larguraDispositivos = Gdx.graphics.getWidth();
-		alturaDispositivo = Gdx.graphics.getHeight();
-		posicaoInicialVertical = alturaDispositivo / 2;
+		inicializarTexturas();
+		inicializarObjetos();
 	}
 
 	@Override
 	public void render () {
 
-		batch.begin();//inicio
+		verificarEstadodoJogo();
+		desenharTexturas();
 
-		if (variacao > 3)//reseta o valor da variação para limitar apenas as 3 imagens do array de passaros
-			variacao = 0;
+	}
+
+	private void verificarEstadodoJogo(){
+
+
 
 		//Aplicar eventos de click
 		boolean toqueTela = Gdx.input.justTouched();
@@ -58,15 +56,49 @@ public class Jogo extends ApplicationAdapter {
 		if (posicaoInicialVertical > 0 || toqueTela )
 			posicaoInicialVertical = posicaoInicialVertical - gravidade;
 
-		batch.draw(fundo, 0, 0, larguraDispositivos , alturaDispositivo );//parametros passado = 1 imagen, 2 eixo x, 3 eixo y, 4 altura, 5 largura
-		batch.draw(passaros.get((int) variacao), 30, posicaoInicialVertical);
-
 		variacao += Gdx.graphics.getDeltaTime() * 10;
 
+		//Verifica a variação para bater asas dp pássaro
+		if (variacao > 3)//reseta o valor da variação para limitar apenas as 3 imagens do array de passaros
+			variacao = 0;
+
 		gravidade++;
-		movimentoX++;
-		movimentoY++;
+
+	}
+
+	private void desenharTexturas(){
+
+		batch.begin();//inicio
+
+		batch.draw(fundo, 0, 0, larguraDispositivos , alturaDispositivo );//parametros passado = 1 imagen, 2 eixo x, 3 eixo y, 4 altura, 5 largura
+		batch.draw(passaros.get((int) variacao), 30, posicaoInicialVertical);
+		batch.draw(canoBaixo, posicaoCanoHorizontal - 100, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos/2);
+		batch.draw(canoTopo, posicaoCanoHorizontal -100, alturaDispositivo / 2 + espacoEntreCanos / 2);
+
 		batch.end();//fim
+
+	}
+
+	private void inicializarTexturas(){
+
+		passaros.add(new Texture("passaro1.png"));
+		passaros.add(new Texture("passaro2.png"));
+		passaros.add(new Texture("passaro3.png"));
+
+		fundo = new Texture("fundo.png");
+		canoBaixo = new Texture("cano_baixo_maior.png");
+		canoTopo = new Texture("cano_topo_maior.png");
+	}
+
+	private void inicializarObjetos(){
+
+		batch = new SpriteBatch();
+
+		larguraDispositivos = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+		posicaoInicialVertical = alturaDispositivo / 2;
+		posicaoCanoHorizontal = larguraDispositivos;
+		espacoEntreCanos = 150;
 	}
 	
 	@Override
